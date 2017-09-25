ea104653ead006d0ee4cc5c221b44f89cb1d1e6a
Emanuel Calderoni <emanuelcalderoni@MacBook-Pro-de-Emanuel.local>
2017-09-25 14:47:04 -0300

    Inserta un cuadro rojo arriba a la izquierda

--- GitHubProject/ViewController.swift
+++ GitHubProject/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let cuadro = UIView(frame: CGRect(x : 0, y: 050, width: 50, height: 50))
+        cuadro.backgroundColor = UIColor.red
+        view.addSubview(cuadro)
     }
 
     override func didReceiveMemoryWarning() {
